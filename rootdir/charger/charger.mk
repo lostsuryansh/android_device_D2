12da43dadcb464feb77bf9a528e81deadcb5fdc8
lostsuryansh <lostsuryansh@gmail.com>
2021-08-07 08:24:18 +0530

    D2: Update fstab for Android-10

---
+++ rootdir/charger/charger.mk
@@ -0,0 +1,21 @@
+PRODUCT_COPY_FILES += \
+    device/10or/D2/rootdir/charger/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
+    device/10or/D2/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    device/10or/D2/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    device/10or/D2/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    device/10or/D2/rootdir/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    device/10or/D2/rootdir/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    device/10or/D2/rootdir/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    device/10or/D2/rootdir/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    device/10or/D2/rootdir/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    device/10or/D2/rootdir/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    device/10or/D2/rootdir/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    device/10or/D2/rootdir/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    device/10or/D2/rootdir/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    device/10or/D2/rootdir/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    device/10or/D2/rootdir/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    device/10or/D2/rootdir/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    device/10or/D2/rootdir/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    device/10or/D2/rootdir/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    device/10or/D2/rootdir/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    device/10or/D2/rootdir/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png
